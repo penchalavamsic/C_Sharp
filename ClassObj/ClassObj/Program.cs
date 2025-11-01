a7b212efa27d0e9da265d81026b6e10720d3caba
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-01 17:30:21 +0530

    simple class and obj access

---
+++ ClassObj/ClassObj/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObj
+{
+    public class Car {
+        public string name()
+        {
+            string a = "Benz";
+            return a;
+        }
+        public string name1{set; get;}="BMW"; 
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myobj = new Car();
+            Console.WriteLine(myobj.name()); //accesing method name from class Car
+            Console.WriteLine(myobj.name1); //accesing property name1 from class Car
+        }
+    }
+}
