7f44dc259fdb5a1de77670bd69b1cbd703c853c3
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-02 18:24:33 +0530

    List with operations

---
+++ Collections/List/List/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating list
+            List<int> numbers = new List<int>();
+            //adding elements
+            for(int i = 0; i < 10; i++)
+            {
+                numbers.Add(i);
+            }
+            //finding elements
+            if(numbers.Contains(5))
+            {
+                Console.WriteLine("5 is found in the list at: "+numbers.IndexOf(5));
+            }
+            //foreach for printing elements
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
