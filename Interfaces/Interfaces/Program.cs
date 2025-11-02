61110072dd5b15bed075c5e53ca9191833653f7e
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-02 18:06:18 +0530

    Interfaces

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    //interface
+    public interface  ICar
+    {
+        void Company();
+    }
+    //not need to write override keyword
+    //multiple interface inheritance is possible
+    public class Name : ICar
+    {
+               public void Company()
+        {
+            Console.WriteLine("BMW");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Name n = new Name();
+            n.Company();
+           
+        }
+    }
+}
