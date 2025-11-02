ec544938a8f2d51ad9bb9613d792c53520e693f9
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-02 17:40:59 +0530

    single inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    //base class
+    public class Animal {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }    
+    }
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.Eat(); //Inherited method
+            d.Bark(); //Own method
+        }
+    }
+}
