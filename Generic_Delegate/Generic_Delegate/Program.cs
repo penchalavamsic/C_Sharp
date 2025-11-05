6f1e611f77a6070cc58f3e1abf0994856d91ce0a
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-05 22:42:12 +0530

    generics with delegates

---
+++ Generic_Delegate/Generic_Delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Delegate
+//even or odd with generic delegate
+{
+    internal class Program
+    {
+        public delegate void Mydelegate<T>(T a); //even or odd with generic delegate
+        public static void EvenorOdd(int b)
+        {
+            if(b % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Odd Number");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Mydelegate<int> del = EvenorOdd; //assigning method to delegate
+            del(5); //calling delegate
+
+        }
+    }
+}
