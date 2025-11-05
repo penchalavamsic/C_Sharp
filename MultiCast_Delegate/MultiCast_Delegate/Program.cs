2583b4c6e954c5b7217b5c98a03b9589e265f714
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-05 22:23:28 +0530

    Multicast delegate with parameters

---
+++ MultiCast_Delegate/MultiCast_Delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiCast_Delegate
+{
+    //multicast delegate
+    internal class Program
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int sub(int a, int b)
+        {
+            return a - b;
+        }
+        //delegate declaration
+        public delegate int mydelegate(int a, int b);
+        static void Main(string[] args)
+        {
+            //delegate instantiation
+            mydelegate del = add;
+            del += sub;
+            int result = del(10, 5);
+            Console.WriteLine("Result: " + result);
+            }
+    }
+}
