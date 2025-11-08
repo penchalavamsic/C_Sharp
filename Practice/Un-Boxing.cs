f2166fb14785518f8a2f0ce68507d7f3cfd9c7ba
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-08 15:41:56 +0530

    boxing & unboxing

---
+++ Practice/Un-Boxing.cs
@@ -0,0 +1,10 @@
+//boxing and unboxing
+using System;
+class Program{
+    int a=10; //type int
+    obj o=a //type object reference -stored in heap
+    int b=(int)o; //unboxing type casting -stored in stack
+
+
+
+}
