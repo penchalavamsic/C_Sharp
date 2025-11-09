a0bd6ce3e0c040e34ad57d606cd1e23462035a32
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-09 22:29:14 +0530

    multi-threading with wait, start

---
+++ Threading/Threading/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    
+    internal class Program
+    {
+        static void even()
+        {
+            for (int i = 0; i <= 10; i += 2)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500); //for waiiting 500 milliseconds
+            }
+        }
+        static void odd()
+        {
+            for (int i = 1; i < 10; i += 2)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500); //for waiiting 500 milliseconds
+            } 
+        }
+        static void Main(string[] args)
+        {
+            Thread t1=new Thread(new ThreadStart(even));
+            Thread t2=new Thread(new ThreadStart(odd));
+            t1.Start(); //starting the thread
+            t2.Start();
+            t1.Join(); //waiting for the thread to complete its execution
+            t2.Join();
+        }
+    }
+}
